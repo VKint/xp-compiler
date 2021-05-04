55e816e71a3aa3d0ca74ea4d4e1d147280a189d8
VKint <kint@xp.network>
2021-05-04 20:00:40 +0530

    tests: move compile: add comments

--- src/tests.rs
+++ src/tests.rs
@@ -57,11 +57,16 @@ fn test_solidity_transfer_amount() {
 
 #[test]
 fn test_move_deserialize() {
+    // Import bytecode from pre-compiled move template
     let bytedata = include_bytes!("../assets/call.mv");
+    // deserialize it to opcodes
     let script = CompiledScript::deserialize(bytedata).unwrap();
     let code = MoveCode::new_no_mods(script);
+
+    // Convert it to our interface call
     let call = XpCallJson::from_move(&code).unwrap();
 
+    // Compile solidity code
     let compile = call.compile().unwrap();
     if cfg!(feature = "test_generated") {
         println!("{}", compile);
